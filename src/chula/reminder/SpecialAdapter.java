c33b68e400d31d2f47af63af57042c07ac0c7c67
Boss <cu.nullify@gmail.com>
2012-10-05 17:05:26 +0700

    More easy to look

--- src/chula/reminder/SpecialAdapter.java
+++ src/chula/reminder/SpecialAdapter.java
@@ -9,6 +9,7 @@ import android.view.View;
 import android.view.ViewGroup;
 import android.widget.ArrayAdapter;
 import android.widget.SimpleAdapter;
+import android.widget.TextView;
 
 public class SpecialAdapter extends ArrayAdapter<String> {
 	private int[] colors = new int[] { 0x30FF0000, 0x300000FF };
@@ -23,6 +24,8 @@ public class SpecialAdapter extends ArrayAdapter<String> {
 	@Override
 	public View getView(int position, View convertView, ViewGroup parent) {
 	    View v = super.getView(position, convertView, parent);
+	    TextView textView=(TextView) v.findViewById(android.R.id.text1);
+	    textView.setTextColor(Color.BLACK);
 	    Factor factor=factors.get(position);
 	    if(factor.isLate()){
 	    	v.setBackgroundColor(Color.RED);
